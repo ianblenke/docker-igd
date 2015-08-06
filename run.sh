2900efa5cbdf479e70968d75df327654672e3def
Ian Blenke <ian@blenke.com>
2015-08-06 14:27:24 -0400

    Initial checkin

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+EXTIFACE=${EXTIFACE:-eth0}
+INTIFACE=${INTIFACE:-docker0}
+
+grep -e "^EXTIFACE=" /etc/default/linux-igd || \
+  echo "EXTIFACE=$(EXTIFACE)" >> /etc/default/linux-igd
+
+grep -e "^INTIFACE=" /etc/default/linux-igd || \
+  echo "INTIFACE=${INTIFACE}" >> /etc/default/linux-igd
+
+sed -i -e "s/^EXTIFACE=.*\$/EXTIFACE=${EXTIFACE}/" \
+       -e "s/^INTIFACE=.*\$/INTIFACE=${INTIFACE}/" \
+       /etc/default/linux-igd
+
+. /etc/default/linux-igd
+
+# This is needed to keep running in the foreground in debug mode
+exec /usr/sbin/upnpd -f ${EXTIFACE} ${INTIFACE}
